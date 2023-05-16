30bd38eb72c8e56ca7a4975849c363788ef2892d
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-05-16 15:05:25 +0000

    last commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,37 @@
 const API = "https://sheetdb.io/api/v1/b8p9r56bjqs5y";
 
 const { createApp } = Vue;
+
 const appId = "app";
 
 const objectApp = {
     data: function(){
         return {
-             lineCreate: null,
-        headerImage: null,
-        vino: null,
-        typing: false,
-        data: null,
         vinosAPI: null
         }
-    } ,
+    } 
+    /*
+    ,
 
     created: function(){
-        fetch(API).then(res =>res.json()).then(data => this.vinosAPI =  data)
+        fetch(API) 
+        .then(res => res.json())
+        .then(data => this.vinosAPI = data)
     }
-     
+     */
+    
+    ,
+    methods:{
+        fetchVinos: function(){
+            console.log("data")
+        fetch(API) 
+        .then(res => res.json())
+        .then(data => this.vinosAPI = data)
+        .catch( error => console.log({error}))
 
-        
+        }
+    }
+
    
 };
 
